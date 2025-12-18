e9ba7e1ad5e7c8634765c5dab71bac3382dcd51e
Sietsethooft <147145688+Sietsethooft@users.noreply.github.com>
2025-12-18 14:01:53 +0100

    Login fix

--- lib/login.ts
+++ lib/login.ts
@@ -1,23 +1,28 @@
 import { apiFetch } from "@/utils/apiFetch";
 import { loginResponse } from "../app/types/login";
+import { JsonResponse } from "../app/types/jsonResponse";
 
 export async function loginAPI(
   email: string,
   password: string
 ): Promise<loginResponse> {
-  const data = await apiFetch<loginResponse>("/auth/login", {
+  const response = await apiFetch<JsonResponse<loginResponse>>("/auth/login", {
     method: "POST",
     headers: {
       "Content-Type": "application/json",
     },
     body: JSON.stringify({ email, password }),
-    skipAuth: true, // Dont send token for login
+    skipAuth: true,
   });
 
-  // Store token in localStorage
-  if (typeof window !== "undefined" && data.token) {
-    localStorage.setItem("token", data.token);
+  // Controleer of er een token is, anders is login mislukt
+  if (!response.data.token) {
+    throw new Error("Login failed");
   }
 
-  return data;
+  if (typeof window !== "undefined" && response.data.token) {
+    localStorage.setItem("token", response.data.token);
+  }
+
+  return response.data;
 }
