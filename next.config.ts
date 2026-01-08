9620518caab2e717eb6fb5dfc542bc9699230024
Clemens <100091164+Clemens00505@users.noreply.github.com>
2026-01-08 16:03:19 +0100

    Update Next.js config with dev indicators and allowed origins

--- next.config.ts
+++ next.config.ts
@@ -1,6 +1,12 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
+  devIndicators: {
+    appIsrStatus: false,
+  },
+  experimental: {
+    allowedDevOrigins: ["192.168.2.5", "localhost"],
+  },
 };
 
 export default nextConfig;
